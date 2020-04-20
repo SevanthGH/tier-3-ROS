f3e8e4e93499430bf4197e8e8427995ed06a1bb8
sh-192094 <@sh-192094>
2020-04-20 15:09:53 +0200

    created publisher #4

---
+++ ex04_publisher/src/my_publisher.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import rospy                            # Import the Python library for ROS
+from geometry_msgs.msg import Twist          # Import the Int32 message from the std_msgs
+
+rospy.init_node('topic_publisher')      # Initiate a Node named 'topic_publisher'
+pub = rospy.Publisher('/cmd_vel', Twist) # Create a Publisher object, that will publish
+                                        # messages of type Int32
+
+rate = rospy.Rate(2)                    # Set a publish rate of 2 Hz
+count = Twist()                         # Create a var of type Int32
+count.linear.x = 0                          # Initialize 'count' variable
+
+while not rospy.is_shutdown():          # Create a loop that will go until someone stops the programm                  
+    pub.publish(count)                  # Publish the message within the 'count' variable
+    #count.data += 1                     # Increment 'count' variable
+    rate.sleep()                        # Make sure the publish rate maintains at 2 Hz
+
