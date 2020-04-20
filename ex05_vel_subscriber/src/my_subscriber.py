155edb9e8873b195835927ddba5e20e69fa08d42
sh-192094 <@sh-192094>
2020-04-20 15:10:52 +0200

    created subscriber #5

---
+++ ex05_vel_subscriber/src/my_subscriber.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import Twist
+
+def callback(msg):
+    print msg.linear.x
+
+rospy.init_node('vel_subscriber')
+rospy.Subscriber('/cmd_vel', Twist, callback)
+rospy.spin()
+
