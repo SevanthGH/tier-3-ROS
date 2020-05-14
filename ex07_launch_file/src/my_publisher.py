b0545ff198436e06e2676b3856da89cfddf1bf81
sh-192094 <@sh-192094>
2020-05-14 15:54:26 +0200

    reading goals from the topic goals and points #7

--- ex07_launch_file/src/my_publisher.py
+++ ex07_launch_file/src/my_publisher.py
@@ -5,9 +5,8 @@ import math
 from goal_publisher.msg import PointArray
 
 def callback(msg):
-    #a = len(msg.goals)
     for i in range (len(msg.goals)):
-        print 'point',i,': x: ',msg.goals[i].x, ', y: ',msg.goals[i].y, ', z: ', msg.goals[i].z
+        print 'Point',i,': x: ',msg.goals[i].x, ', y: ',msg.goals[i].y, ', z: ', msg.goals[i].z
 
 rospy.init_node('vel_launch_subscriber')
 rospy.Subscriber('/points', PointArray, callback)
